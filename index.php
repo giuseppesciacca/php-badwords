a9dc7b5021a91801236c3cfcd8777c208571c902
giuseppesciacca <sciaccagi@gmail.com>
2023-05-03 16:30:42 +0200

    [add] string replace on badword

--- index.php
+++ index.php
@@ -13,15 +13,21 @@
 
 <body>
     <h1 class="text-center">PHP Badwords</h1>
+    <h5 class="text-center">La badwords è <span class="bg-danger">'dolor'</span></h5>
     <div class="container my-5">
         <form action="myscript.php" method="post">
             <div class="mb-3">
-              <label for="paragraph" class="form-label">Inserisci paragrafo</label>
+              <label for="paragraph" class="form-label">Inserisci paragrafo. Se vuoi puoi inserire un paragrafo di prova che si trova proprio sotto il form ;)</label>
               <textarea class="form-control mb-3" name="paragraph" id="paragraph" rows="3"></textarea>
               <button type="submit" class="btn btn-primary">Submit</button>
               <button type="reset" class="btn btn-primary">Reset</button>
             </div>
         </form>
+
+        <h3>Paragrafo di prova:</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga similique incidunt voluptate ab molestiae soluta obcaecati tempora corrupti laudantium cupiditate aliquid deleniti officiis, iure ipsam eaque, vel nihil tenetur porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro inventore quo expedita rerum. Quas, error repellat ipsam accusantium odit sed eveniet totam nostrum. Tempore, accusantium numquam commodi incidunt quisquam suscipit?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quisquam veniam autem quidem eum unde corporis illo tempora dolore totam ipsam, harum cupiditate ex voluptatum neque odio incidunt nobis enim?
+        </p>
     </div>
 
 
--- myscript.php
+++ myscript.php
@@ -18,28 +18,33 @@ per lo svolgimento vi occorretanno:
 
 <?php
 $paragraph = $_POST["paragraph"];
+
+$paragraph_censored = str_replace("dolor", "***", $paragraph)
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-paragraph-censored</title>
     <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/discss/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1ZlJr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
+
 <body>
-    <p class="container my-5"><?php echo $paragraph ?></p>
 
+    <h1 class="text-center">Paragrafo censurato. Niente badwords</h1>
+    <p class="container my-5"><?php echo $paragraph_censored ?></p>
 
-
-    
     <!-- Bootstrap JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/distjs/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZ+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
+
