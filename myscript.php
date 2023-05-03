17ded049ca25732970add96d265bf0d710e88d7c
giuseppesciacca <sciaccagi@gmail.com>
2023-05-03 16:35:38 +0200

    [add] paragraph with badword and length on screen

--- myscript.php
+++ myscript.php
@@ -1,25 +1,7 @@
-<!-- Descrizione:
-Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
-Gestire il tutto con due file diversi.
-
-Il primo file (index.php) dovrà permettere all’utente di inserire i dati (tramite un form) e inviare la richiesta al server.
-
-Il secondo file (nome a piacere) riceverà la richiesta ed eseguirà queste operazioni:
-    stampare a schermo il paragrafo e la sua lunghezza
-    stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
-
-Consigli:
-per lo svolgimento vi occorretanno:
-    due file, index.php e myscript.php
-    un form in index.php con gli attributi action e method
-    due campi nel form, per inviare paragrafo e parola da censurare (ricordatevi di usare l'attributo name )
-    ricercare metodo php per sostituire una porzione di una stringa (prova a googlare: string replace in php)
-    ricercare un metodo php per contare la lunghezza di una stringa (prova a googlare string.length in php). -->
-
 <?php
 $paragraph = $_POST["paragraph"];
 
-$paragraph_censored = str_replace("dolor", "***", $paragraph)
+$paragraph_censored = str_replace("dolor", "*****", $paragraph)
 ?>
 
 <!DOCTYPE html>
@@ -37,8 +19,13 @@ $paragraph_censored = str_replace("dolor", "***", $paragraph)
 
 <body>
 
-    <h1 class="text-center">Paragrafo censurato. Niente badwords</h1>
+    <h2 class="text-center">Paragrafo con badwords</h2>
     <div class="container my-5">
+        <p><?php echo $paragraph ?></p>
+        <p>Il paragrafo è lungo <?php echo strlen($paragraph) ?> parole</p>
+
+
+    <h2 class="text-center">Paragrafo censurato. Niente badwords</h2>
         <p><?php echo $paragraph_censored ?></p>
         <p>Il paragrafo è lungo <?php echo strlen($paragraph_censored) ?> parole</p>
     </div>
